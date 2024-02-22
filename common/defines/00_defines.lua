af71d7defc1f23b9a99ba8704666de20587d28c2
Johbii <30395979+Johbii@users.noreply.github.com>
2024-02-22 02:07:51 -0800

    Increase ticking warscore bonus x3

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -1,2 +1,3 @@
-NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 6 -- -x10 months before war leader full occupation = 100% war score
-NDefines.NMilitary.GARRISON_SIZE = 2000                -- x2 garrison size (base 1000)
+NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 6         -- -x10 months before war leader full occupation = 100% war score
+NDefines.NMilitary.GARRISON_SIZE = 2000                        -- x2 garrison size (base 1000)
+NDefines.NMilitary.DEFAULT_WARGOAL_TICKINGWARSCORE_BONUS = 1.6 -- x3 faster ticking warscore (def 0.4)
