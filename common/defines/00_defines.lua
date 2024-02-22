900d2427b95a39e97c116e89fbc16d3ed7aba0e7
Johbii <30395979+Johbii@users.noreply.github.com>
2024-02-22 01:30:35 -0800

    Double base garrison size constant

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -1,1 +1,2 @@
-NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 6
+NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 6 -- -x10 months before war leader full occupation = 100% war score
+NDefines.NMilitary.GARRISON_SIZE = 2000                -- x2 garrison size (base 1000)
