0d08c52a845e0257fbd2dfec35f20b924b4b6268
Johbii <30395979+Johbii@users.noreply.github.com>
2024-03-16 16:06:18 -0700

    Reduce force needed to siege due to garrison size

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -1,3 +1,4 @@
 NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 6         -- -x10 months before war leader full occupation = 100% war score
 NDefines.NMilitary.GARRISON_SIZE = 2000                        -- x2 garrison size (base 1000)
+NDefines.NMilitary.SIEGE_FORCE_NEEDED_MULTIPLIER = 1.5         -- x0.5 force needed to siege (due to garrison size doubling) (base 3.0)
 NDefines.NMilitary.DEFAULT_WARGOAL_TICKINGWARSCORE_BONUS = 1.6 -- x3 faster ticking warscore (def 0.4)
