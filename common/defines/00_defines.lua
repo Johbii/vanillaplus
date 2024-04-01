c097e713fd64e45556f2ae549885a5098fb457ad
Johbii <30395979+Johbii@users.noreply.github.com>
2024-03-31 21:05:18 -0700

    Increase months before full occupation =100% WS

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -1,4 +1,4 @@
-NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 6         -- -x10 months before war leader full occupation = 100% war score
+NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 24        -- -x2.5 months before war leader full occupation = 100% war score
 NDefines.NMilitary.GARRISON_SIZE = 3000                        -- x3 garrison size (base 1000)
 NDefines.NMilitary.SIEGE_FORCE_NEEDED_MULTIPLIER = 0.33        -- x0.33 force needed to siege (due to garrison size doubling) (base 3.0)
 NDefines.NMilitary.DEFAULT_WARGOAL_TICKINGWARSCORE_BONUS = 1.6 -- x3 faster ticking warscore (def 0.4)
