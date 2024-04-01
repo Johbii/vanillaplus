477d34894e56312589f95bb3700f762183791656
Johbii <30395979+Johbii@users.noreply.github.com>
2024-03-31 17:40:00 -0700

    Reduce fortress cost according to new garrison

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -2,3 +2,4 @@ NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 6         -- -x10 months be
 NDefines.NMilitary.GARRISON_SIZE = 3000                        -- x3 garrison size (base 1000)
 NDefines.NMilitary.SIEGE_FORCE_NEEDED_MULTIPLIER = 0.33        -- x0.33 force needed to siege (due to garrison size doubling) (base 3.0)
 NDefines.NMilitary.DEFAULT_WARGOAL_TICKINGWARSCORE_BONUS = 1.6 -- x3 faster ticking warscore (def 0.4)
+NDefines.NMilitary.FORTRESS_COST = 0.166
