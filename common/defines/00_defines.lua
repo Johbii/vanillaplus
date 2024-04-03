adffa2b7e4bf0987e9c00cbe44538f448308f759
Johbii <30395979+Johbii@users.noreply.github.com>
2024-04-03 14:11:31 -0700

    Prevent uncon from effecting call for peace value

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -2,4 +2,5 @@ NDefines.NDiplomacy.MONTHS_BEFORE_TOTAL_OCCUPATION = 24        -- -x2.5 months b
 NDefines.NMilitary.GARRISON_SIZE = 3000                        -- x3 garrison size (base 1000)
 NDefines.NMilitary.SIEGE_FORCE_NEEDED_MULTIPLIER = 0.33        -- x0.33 force needed to siege (due to garrison size doubling) (base 3.0)
 NDefines.NMilitary.DEFAULT_WARGOAL_TICKINGWARSCORE_BONUS = 1.6 -- x3 faster ticking warscore (def 0.4)
-NDefines.NMilitary.FORTRESS_COST = 0.166
+NDefines.NMilitary.FORTRESS_COST = 0.166                       -- -x3 fortress upkeep cost from garrison size (def 0.5)
+NDefines.NDiplomacy.UNCONDITIONAL_SURRENDER_MONTHS = -1        -- remove unconditional surrender contribution to call for peace value (def 2)
